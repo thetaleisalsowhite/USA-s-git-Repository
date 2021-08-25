6e8d8635549f69130c16dfd5e7bb97165a25e4c9
thetaleisalsowhite <doraemo104@gmail.com>
2021-08-25 22:53:41 +0900

    2021/08/25/22:53

---
+++ USA-s-program-practice/C-language/05if-conditional_branch.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/* 
+
+ðŒ•ªŠò(if)
+>= 
+<=
+>
+<
+==
+!=
+
+*/
+
+int main(void) {
+
+    int score = 55;
+    
+    if (score >= 60) {
+        printf("OK!\n");
+    } else if (score >= 50) {
+        printf("so so...\n");
+    } else {
+        printf("NG!\n");
+    }
+
+    return 0;
+}
